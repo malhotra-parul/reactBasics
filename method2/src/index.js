cb441ea71ac71ad507cb7b0ef7cee4bd27277d2c
malhotra-parul <parulm.business@gmail.com>
2020-01-07 22:22:16 +0530

    Encapsulated Clock.js

--- method2/src/index.js
+++ method2/src/index.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./component/App";
+//import Clock from "./component/Clock";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
 
 
