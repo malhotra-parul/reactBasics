cb2c764e51b921173c4e7906404b4a0b80b12381
malhotra-parul <parulm.business@gmail.com>
2020-01-11 17:10:05 +0530

    Using inline && operator

---
+++ method2/src/component/Mailbox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Clock from "./component/Clock";
+// import ActionClick from "./component/ActionLink";
+// import Toggle from "./component/Toggle";
+//import Greeting from "./component/Greeting";
+import LoginControl from "./component/LoginControl";
+
+
+ReactDOM.render(<LoginControl />, document.getElementById("main"));
